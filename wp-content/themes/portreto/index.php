c83a995b42c0a6d672745757f4c0bbfeeada74d2
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-04-19 21:59:18 +0100

    Adição do template da página home

--- wp-content/themes/portreto/index.php
+++ wp-content/themes/portreto/index.php
@@ -1,5 +1,122 @@
-<?php
-    get_header();
-    get_sidebar();
-    get_footer();
-?>
+<?php get_header(); ?>
+    
+    <!-- content 
+			================================================== -->
+		<div id="content">
+
+			<!-- carousel works section -->
+			<section class="carousel-works">
+				
+				<div class="works-box owl-wrapper">
+					<div class="owl-carousel" data-num="4">
+					
+						<div class="item">
+							<div class="works-post">
+								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/1.jpg' ?>" alt="">
+								<div class="hover-box">
+									<div class="inner-hover">
+										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+										<h2><a href="single-project.html">Nature tincidunt</a></h2>
+										<ul class="project-tags">
+											<li><a href="#">Portrait,</a></li>
+											<li><a href="#">Lifestyle</a></li>
+										</ul>
+										<div class="hidden-box">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+											<a href="single-project.html">View Post</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="works-post">
+								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/2.jpg' ?>" alt="">
+								<div class="hover-box">
+									<div class="inner-hover">
+										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+										<h2><a href="single-project.html">Nature tincidunt</a></h2>
+										<ul class="project-tags">
+											<li><a href="#">Portrait,</a></li>
+											<li><a href="#">Lifestyle</a></li>
+										</ul>
+										<div class="hidden-box">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+											<a href="single-project.html">View Post</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="works-post">
+								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/3.jpg' ?>" alt="">
+								<div class="hover-box">
+									<div class="inner-hover">
+										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+										<h2><a href="single-project.html">Nature tincidunt</a></h2>
+										<ul class="project-tags">
+											<li><a href="#">Portrait,</a></li>
+											<li><a href="#">Lifestyle</a></li>
+										</ul>
+										<div class="hidden-box">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+											<a href="single-project.html">View Post</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="works-post">
+								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/4.jpg' ?>" alt="">
+								<div class="hover-box">
+									<div class="inner-hover">
+										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+										<h2><a href="single-project.html">Nature tincidunt</a></h2>
+										<ul class="project-tags">
+											<li><a href="#">Portrait,</a></li>
+											<li><a href="#">Lifestyle</a></li>
+										</ul>
+										<div class="hidden-box">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+											<a href="single-project.html">View Post</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="works-post">
+								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/5.jpg' ?>" alt="">
+								<div class="hover-box">
+									<div class="inner-hover">
+										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+										<h2><a href="single-project.html">Nature tincidunt</a></h2>
+										<ul class="project-tags">
+											<li><a href="#">Portrait,</a></li>
+											<li><a href="#">Lifestyle</a></li>
+										</ul>
+										<div class="hidden-box">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
+											<a href="single-project.html">View Post</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
+			</section>
+			<!-- end carousel works section -->
+
+		</div>
+		<!-- End content -->
+    
+<?php get_footer(); ?>
