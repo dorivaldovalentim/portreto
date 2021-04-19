8c4a75304c2eaab46d29d512cbbdb0c14922a91f
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-04-20 00:38:28 +0100

    Adição da página galleries

---
+++ wp-content/themes/portreto/page-galleries.php
@@ -0,0 +1,191 @@
+<?php get_header(); ?>
+
+    <!-- content 
+		================================================== -->
+		
+        <div id="content">
+
+			<!-- portfolio section -->
+			<section class="portfolio-section">
+				<a class="filter-toggle" href="#">
+                    <i class="fa fa-align-justify" aria-hidden="true"></i>
+                </a>
+				
+                <ul class="filter">
+					<li><a class="active" href="#" data-filter="*">all</a></li>
+					<li><a href="#" data-filter=".photography">photography</a></li>
+					<li><a href="#" data-filter=".model">model</a></li>
+					<li><a href="#" data-filter=".nature">nature</a></li>
+					<li><a href="#" data-filter=".portrait">portrait</a></li>
+				</ul>
+
+				<div class="portfolio-box iso-call col-3">
+					<div class="project-post photography nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p1.jpg' ?>" alt="">
+						<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p2.jpg' ?>" alt="">
+						<a class="likes" href="#">18 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post model portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p3.jpg' ?>" alt="">
+						<a class="likes" href="#">1 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post nature portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p4.jpg' ?>" alt="">
+						<a class="likes" href="#">23 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography model">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p5.jpg' ?>" alt="">
+						<a class="likes" href="#">15 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p6.jpg' ?>" alt="">
+						<a class="likes" href="#">10 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post model nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p7.jpg' ?>" alt="">
+						<a class="likes" href="#">55 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p8.jpg' ?>" alt="">
+						<a class="likes" href="#">13 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post model">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p9.jpg' ?>" alt="">
+						<a class="likes" href="#">12 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p10.jpg' ?>" alt="">
+						<a class="likes" href="#">26 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p11.jpg' ?>" alt="">
+						<a class="likes" href="#">29 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post model nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p12.jpg' ?>" alt="">
+						<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p13.jpg' ?>" alt="">
+						<a class="likes" href="#">4 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p14.jpg' ?>" alt="">
+						<a class="likes" href="#">45 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post photography model nature">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p15.jpg' ?>" alt="">
+						<a class="likes" href="#">44 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+					<div class="project-post portrait">
+						<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/p16.jpg' ?>" alt="">
+						<a class="likes" href="#">9 <i class="fa fa-heart-o"></i></a>
+						<div class="hover-box">
+							<div class="inner-hover">
+								<h2><a href="#">Pellentesque odio</a></h2>
+								<span>Nov 4th, 2015, 0 Comments</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- end portfolio section -->
+
+		</div>
+	
+    <!-- End content -->
+
+<?php get_footer(); ?>
