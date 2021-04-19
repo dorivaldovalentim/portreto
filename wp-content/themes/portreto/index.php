d04d0bcb0870f172aac9993ab35159af856fc97b
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-04-20 00:14:40 +0100

    Tornando a zona de destaques na home dinâmica

--- wp-content/themes/portreto/index.php
+++ wp-content/themes/portreto/index.php
@@ -8,109 +8,51 @@
 			<section class="carousel-works">
 				
 				<div class="works-box owl-wrapper">
+
 					<div class="owl-carousel" data-num="4">
 					
-						<div class="item">
-							<div class="works-post">
-								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/1.jpg' ?>" alt="">
-								<div class="hover-box">
-									<div class="inner-hover">
-										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
-										<h2><a href="single-project.html">Nature tincidunt</a></h2>
-										<ul class="project-tags">
-											<li><a href="#">Portrait,</a></li>
-											<li><a href="#">Lifestyle</a></li>
-										</ul>
-										<div class="hidden-box">
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-											<a href="single-project.html">View Post</a>
+						<?php query_posts( 'post_type=carousel' ); ?>
+
+						<?php while( have_posts() ): the_post() ?>
+
+							<div class="item">
+
+								<div class="works-post">
+
+									<?= the_post_thumbnail() ?>
+
+									<div class="hover-box">
+
+										<div class="inner-hover">
+											<a class="likes" href="#">
+												14 <i class="fa fa-heart-o"></i>
+											</a>
+											
+											<h2><a href="single-project.html"><?= the_title() ?></a></h2>
+
+											<ul class="project-tags">
+												<li><a href="#">Portrait,</a></li>
+												<li><a href="#">Lifestyle</a></li>
+											</ul>
+
+											<div class="hidden-box">
+												<p><?= the_excerpt() ?></p>
+												<a href="single-project.html"><?= the_field('button') ?></a>
+											</div>
 										</div>
+
 									</div>
+
 								</div>
+
 							</div>
-						</div>
-					
-						<div class="item">
-							<div class="works-post">
-								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/2.jpg' ?>" alt="">
-								<div class="hover-box">
-									<div class="inner-hover">
-										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
-										<h2><a href="single-project.html">Nature tincidunt</a></h2>
-										<ul class="project-tags">
-											<li><a href="#">Portrait,</a></li>
-											<li><a href="#">Lifestyle</a></li>
-										</ul>
-										<div class="hidden-box">
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-											<a href="single-project.html">View Post</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-						<div class="item">
-							<div class="works-post">
-								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/3.jpg' ?>" alt="">
-								<div class="hover-box">
-									<div class="inner-hover">
-										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
-										<h2><a href="single-project.html">Nature tincidunt</a></h2>
-										<ul class="project-tags">
-											<li><a href="#">Portrait,</a></li>
-											<li><a href="#">Lifestyle</a></li>
-										</ul>
-										<div class="hidden-box">
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-											<a href="single-project.html">View Post</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-						<div class="item">
-							<div class="works-post">
-								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/4.jpg' ?>" alt="">
-								<div class="hover-box">
-									<div class="inner-hover">
-										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
-										<h2><a href="single-project.html">Nature tincidunt</a></h2>
-										<ul class="project-tags">
-											<li><a href="#">Portrait,</a></li>
-											<li><a href="#">Lifestyle</a></li>
-										</ul>
-										<div class="hidden-box">
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-											<a href="single-project.html">View Post</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					
-						<div class="item">
-							<div class="works-post">
-								<img src="<?= get_template_directory_uri() . '/assets/upload/portfolio/5.jpg' ?>" alt="">
-								<div class="hover-box">
-									<div class="inner-hover">
-										<a class="likes" href="#">14 <i class="fa fa-heart-o"></i></a>
-										<h2><a href="single-project.html">Nature tincidunt</a></h2>
-										<ul class="project-tags">
-											<li><a href="#">Portrait,</a></li>
-											<li><a href="#">Lifestyle</a></li>
-										</ul>
-										<div class="hidden-box">
-											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.</p>
-											<a href="single-project.html">View Post</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						
+						<?php endwhile; ?>
+
+						<?php wp_reset_query(); ?>
 						
 					</div>
+
 				</div>
 
 			</section>
