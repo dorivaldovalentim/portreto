fe3c096d6b2eb409fdaa54ef332b3043fb723007
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-04-24 18:19:00 +0100

    Transformando a página do blog dinâmica

--- wp-content/themes/portreto/page-blog.php
+++ wp-content/themes/portreto/page-blog.php
@@ -7,163 +7,72 @@
 
 			<!-- page-title section -->
 			<section class="page-title-section">
+
 				<div class="container-fluid">
+
 					<ul class="page-pag">
 						<li><a href="#">Blog</a></li>
-						<li><a href="#">Blog Standart</a></li>
 					</ul>
-					<h1>Blog: Standart</h1>
+
+					<h1>Blog</h1>
+
 				</div>
+
 			</section>
 			<!-- End page-title section -->
 
 			<!-- blog section -->
 			<section class="blog-section standart">
+				
 				<div class="container-fluid">
+				
 					<div class="blog-box">
 
-						<div class="blog-post">
-							<div class="post-gallery">
-								<a href="#"><img src="<?= get_template_directory_uri() . '/assets/upload/blog/default/1.jpg' ?>" alt=""></a>
-							</div>
-							<div class="post-content">
-								<span>Ideas</span>
-								<h2><a href="#">Praesent placerat</a></h2>
-								<span>Nov 4th, 2015, <a href="#">2 comments</a></span>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
-								<div class="social-box">
-									<a class="likes" href="#">21 <i class="fa fa-heart-o"></i></a>
-									<ul class="social-icons">
-										<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-										<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+						<?php query_posts( 'post_type=post' ) ?>
+						<?php while( have_posts() ): the_post() ?>
 
-						<div class="blog-post quote-post">
-							<span>Quote</span>
-							<h2>Design is not just what it <br> looks like <br> and feels like.<br> Design is how it works.</h2>
-							<h4>Steve Jobs</h4>
-							<span>Nov 4th, 2015</span>
-						</div>
+							<div class="blog-post">
+								<div class="post-gallery">
+									<a href="<?= the_permalink() ?>">
+										<?= the_post_thumbnail() ?>
+									</a>
+								</div>
 
-						<div class="blog-post">
-							<div class="post-gallery">
-								<div class="flexslider">
-									<ul class="slides">
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/2.jpg' ?>" />
-										</li>
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/3.jpg' ?>" />
-										</li>
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/4.jpg' ?>" />
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div class="post-content">
-								<span>Ideas</span>
-								<h2><a href="#">Donec odio</a></h2>
-								<span>Nov 4th, 2015, <a href="#">2 comments</a></span>
-								<p><span>Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. <br> Suspendisse potenti.</span></p>
-								<p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. </p>
-								<div class="social-box">
-									<a class="likes" href="#">21 <i class="fa fa-heart-o"></i></a>
-									<ul class="social-icons">
-										<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-										<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+								<div class="post-content">
+									<?php foreach( get_categories() as $category ): ?>
+										<span><?= $category->name ?></span>
+									<?php endforeach; ?>
 
-						<div class="blog-post">
-							<div class="post-gallery">
-								<!-- Vimeo -->
-								<iframe class="videoembed" src="http://player.vimeo.com/video/95746815?title=0&amp;byline=0&amp;portrait=0" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
-								<!-- End Vimeo -->
-							</div>
-							<div class="post-content">
-								<span>Travel</span>
-								<h2><a href="#">Pellentesque odio nisi</a></h2>
-								<span>Nov 4th, 2015, <a href="#">4 comments</a></span>
-								<p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. </p>
-								<div class="social-box">
-									<a class="likes" href="#">21 <i class="fa fa-heart-o"></i></a>
-									<ul class="social-icons">
-										<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-										<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+									<h2>
+										<a href="<?= the_permalink() ?>">
+											<?= the_title() ?>
+										</a>
+									</h2>
 
-						<div class="blog-post">
-							<div class="post-content">
-								<span>Travel</span>
-								<h2><a href="#">Morbi interdum mollis sapien</a></h2>
-								<span>Nov 4th, 2015, <a href="#">4 comments</a></span>
-								<p>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi. Morbi interdum mollis sapien. 
-								<br> Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. </p>
-								<div class="social-box">
-									<a class="likes" href="#">21 <i class="fa fa-heart-o"></i></a>
-									<ul class="social-icons">
-										<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-										<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+									<span>
+										<?= the_date() ?> ||
 
-						<div class="blog-post">
-							<div class="post-gallery">
-								<div class="flexslider">
-									<ul class="slides">
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/4.jpg' ?>" />
-										</li>
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/5.jpg' ?>" />
-										</li>
-										<li>
-											<img alt="" src="<?= get_template_directory_uri() . '/assets/upload/blog/default/6.jpg' ?>" />
-										</li>
-									</ul>
+										<a href="#">
+											<?= comments_number() ?>
+										</a>
+									</span>
+
+									<p><?= the_excerpt() ?></p>
+									
+									<div class="social-box">
+										<ul class="social-icons">
+											<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+											<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+											<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+											<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
+										</ul>
+									</div>
 								</div>
 							</div>
-							<div class="post-content">
-								<span>Travel</span>
-								<h2><a href="#">Cras iaculis ultricies nulla</a></h2>
-								<span>Nov 4th, 2015, <a href="#">2 comments</a></span>
-								<p>Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti. <br>
-								Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. </p>
-								<div class="social-box">
-									<a class="likes" href="#">21 <i class="fa fa-heart-o"></i></a>
-									<ul class="social-icons">
-										<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-										<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-share-alt"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+
+						<?php endwhile; ?>
+
+						<?php wp_reset_query(); ?>
 
 						<ul class="pagination-list">
 							<li><a class="prev" href="#"><i class="fa fa-angle-left"></i></a></li>
